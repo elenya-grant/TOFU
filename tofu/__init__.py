199776e71f832f8c49fe09b429725618860a5540
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2024-12-18 10:51:00 -0700

    updated output dir name

--- tofu/__init__.py
+++ tofu/__init__.py
@@ -4,4 +4,5 @@ TOFU_DIR = Path(__file__).resolve().parent
 ROOT_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = ROOT_DIR/"data"
 INPUT_DIR = ROOT_DIR/"input"
-OUTPUT_DIR = ROOT_DIR/"outputs"
+# OUTPUT_DIR = ROOT_DIR/"outputs"
+OUTPUT_DIR = ROOT_DIR/"results"
