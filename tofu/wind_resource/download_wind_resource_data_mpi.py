fcad6a424e74da1f10677a52347d4e54ca0834ec
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2025-02-07 16:51:04 -0700

    updated code for running multiple hub-heights

--- tofu/wind_resource/download_wind_resource_data_mpi.py
+++ tofu/wind_resource/download_wind_resource_data_mpi.py
@@ -6,14 +6,45 @@ from mpi4py import MPI
 from datetime import datetime
 import logging
 import faulthandler
-from tofu.utilities.file_utilities import check_create_folder,load_yaml
+# from tofu.utilities.file_utilities import check_create_folder,load_yaml
 faulthandler.enable()
 import copy
 from tofu.utilities.tofu_loggers import tofu_logger as tofu_log
 from tofu.wind_resource.get_save_wind_resource_data import download_wind_resource_for_single_site
+
 def do_something(sitelist,match_id,input_cnfg):
-    
-    download_wind_resource_for_single_site(sitelist,match_id,input_cnfg["output_dir"],resource_year=input_cnfg["resource_year"],lat_string=input_cnfg["lat_string"],lon_string=input_cnfg["lon_string"])
+    if "hub_ht_str" in input_cnfg.keys():
+        if isinstance(input_cnfg["hubt_ht_str"],list):
+            for hh_str in input_cnfg["hub_ht_str"]:
+                download_wind_resource_for_single_site(
+                    sitelist,
+                    match_id,
+                    input_cnfg["output_dir"],
+                    resource_year=input_cnfg["resource_year"],
+                    lat_string=input_cnfg["lat_string"],
+                    lon_string=input_cnfg["lon_string"],
+                    hub_height_str = hh_str
+                )
+        else:
+            download_wind_resource_for_single_site(
+                sitelist,
+                match_id,
+                input_cnfg["output_dir"],
+                resource_year=input_cnfg["resource_year"],
+                lat_string=input_cnfg["lat_string"],
+                lon_string=input_cnfg["lon_string"],
+                hub_height_str = input_cnfg["hubt_ht_str"],
+            )
+    else:
+        download_wind_resource_for_single_site(
+                sitelist,
+                match_id,
+                input_cnfg["output_dir"],
+                resource_year=input_cnfg["resource_year"],
+                lat_string=input_cnfg["lat_string"],
+                lon_string=input_cnfg["lon_string"]
+        )
+
     
 
 start_time = datetime.now()
@@ -22,6 +53,7 @@ comm = MPI.COMM_WORLD
 size = MPI.COMM_WORLD.Get_size()
 rank = MPI.COMM_WORLD.Get_rank()
 name = MPI.Get_processor_name()
+
 
 def main(sitelist,input_info,verbose = False):
     site_idxs = sitelist.index
@@ -71,12 +103,12 @@ def main(sitelist,input_info,verbose = False):
         do_something(sitelist,matchid,inputs_copied)
     if verbose:
         print(f"rank {rank}: ellapsed time: {datetime.now() - start_time}")
-    tofu_log.debug.info(f"rank {rank}: ellapsed time: {datetime.now() - start_time}")
-
+    tofu_log.debug(f"rank {rank}: ellapsed time: {datetime.now() - start_time}")
 
 if __name__=="__main__":
     layout = "3x7"
-    wind_sitelist_fname = f"wind_sites_for_resource_download_{layout}.pkl"
+    wind_sitelist_fname = f"wind_sites_for_non_original_resource_download_{layout}.pkl"
+    # wind_sitelist_fname = f"wind_sites_for_resource_download_{layout}.pkl"
     wind_sitelist_dir = os.path.join(str(OUTPUT_DIR),os.path.dirname(__file__).split("/")[-1])
     wind_sitelist_fpath = os.path.join(wind_sitelist_dir,wind_sitelist_fname)
     sitelist = pd.read_pickle(wind_sitelist_fpath)
@@ -88,6 +120,7 @@ if __name__=="__main__":
         "resource_year":2012,
         "lat_string":"parcel_latitude",
         "lon_string":"parcel_longitude",
+        "hub_ht_str": ["turbine_1 hub_height","turbine_2 hub_height"],
         }
     main(sitelist,input_dict)
     
--- tofu/wind_resource/get_save_wind_resource_data.py
+++ tofu/wind_resource/get_save_wind_resource_data.py
@@ -2,11 +2,19 @@ from tofu.site_resource_analysis.wind_resource import WindResource
 import os
 from tofu.utilities.file_utilities import dump_pickle
 
-def download_wind_resource_for_single_site(sitelist,match_id,output_dir,resource_year=2012,lat_string = "parcel_latitude",lon_string="parcel_longitude"):
+def download_wind_resource_for_single_site(
+    sitelist,
+    match_id,
+    output_dir,
+    resource_year=2012,
+    lat_string = "parcel_latitude",
+    lon_string="parcel_longitude", 
+    hub_height_str = "hub_height"
+    ):
     lat = sitelist.loc[match_id][lat_string]
     lon = sitelist.loc[match_id][lon_string]
     gid = sitelist.loc[match_id]["WTK gid"]
-    hub_height = sitelist.loc[match_id]["hub_height"]
+    hub_height = sitelist.loc[match_id][hub_height_str]
 
     output_filename = f"{match_id}-{resource_year}-{hub_height}m.pkl"
     output_filepath = os.path.join(output_dir,output_filename)
